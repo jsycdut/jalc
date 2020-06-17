78ee6172e71d21938dc0c4a9f19e4e2b700d5dd9
jsycdut <jsycdut@gmail.com>
2020-06-17 22:48:08 +0800

    refactor: rename files
    1. aliasrc => aliasrc.sh
    2. envs => envs.sh

--- .config/envs/envs.sh
+++ .config/envs/envs.sh
@@ -1,10 +1,12 @@
+#!/usr/bin/env bash
+
 # proxy
 export http_proxy="http://127.0.0.1:8118"
 export https_proxy="http://127.0.0.1:8118"
 
 # tools
 export EDITOR="nvim"
-#export TERM="st-256color"
+export TERM="st-256color"
 
 # PATH
 export PATH=$PATH:$HOME/.local/bin
