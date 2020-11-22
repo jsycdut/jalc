a3d8aa1e978a95fabca5ee9fcce146a48c1df48a
jsycdut <jsycdut@gmail.com>
2020-11-22 23:42:38 +0800

    feat: update dwm_bar content

--- .local/dwm-bar/dwm_bar.sh
+++ .local/dwm-bar/dwm_bar.sh
@@ -3,16 +3,24 @@
 # just use some command to get desired info string and put them 
 # into the dwm bar in the top right corner
 
-LOC=$(readlink -f "$0")
+LOC=$(dirname $(readlink -f "$0"))
 SCRIPT_DIR="$LOC/funcs"
 
-for script in $SCRIPT_DIR; do
+# load functions in funcs folder
+for script in $SCRIPT_DIR/*; do
   if [[ -x $script ]]; then
-    . $SCRIPT_DIR/script
+    source $script
   fi
 done
 
-#while :; do
-#  xsetroot -name $bar_content
-#  sleep 5
-#done
+DELIMITER=" | "
+
+while :; do
+  bar_content=""
+  bar_content="$bar_content 👉 now $(show_time)"
+  bar_content="$bar_content | $(machine_state)"
+  bar_content="$bar_content | $(alibaba)"
+  xsetroot -name "$bar_content"
+  
+  sleep 5
+done
