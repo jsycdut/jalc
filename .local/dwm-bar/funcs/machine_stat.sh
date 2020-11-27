b74f446b0a82baec2d56d217c7550f491ba1f587
jsycdut <jsycdut@gmail.com>
2020-11-28 04:22:47 +0800

    fix: fix dwm-bar machine_stat script

--- .local/dwm-bar/funcs/machine_stat.sh
+++ .local/dwm-bar/funcs/machine_stat.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+# read /sys/class/power_supply/BAT0/status for battery charing or not
+# read /sys/class/power_supply/BAT0/capacity for battery capacity
+# sometimes you need to read /sys/class/power_supply/BAT0/capacity for battery capacity
+# 
+# use xbacklight to fetch backlight information
+# use amixer for sound speaker status
+# just some simple string combination, dont worry
+
 function machine_state() {
-  battery_left=$(cat /sys/class/power_supply/BAT0/capacity)
-  battery_status=$(cat /sys/class/power_supply/BAT0/status) | tr '[:lower:]' '[:upper:]'
-  if [[ "DISCHARGING" == "$battery_status" ]]; then
-    battery_status='🔻'
-  else
+  battery_status="$(cat /sys/class/power_supply/BAT0/status | tr [:lower:] [:upper:])"
+  if [[ "CHARGING" == "$battery_status" ]]; then
     battery_status='🔺'
+  else
+    battery_status='🔻'
   fi
-  battery_str="$battery_status$battery_left"
-  brightness=$(printf "%.0f\n" $(xbacklight))
-  sound=$(amixer get Master | tail -n 1 | sed -r "s/.*\[(.*)%\].*/\1/")
-  printf "%s%s%s%s%s\n" "$battery_str" " " "🔔$sound" " " "💡$brightness"
+  battery_status="$battery_status$(cat /sys/class/power_supply/BAT0/capacity)"
+  brightness_status="💡$(printf "%.0f\n" $(xbacklight))"
+  sound_status="🔔$(amixer get Master | tail -n 1 | sed -r "s/.*\[(.*)%\].*/\1/")"
+  printf "%s %s %s\n" $battery_status $brightness_status $sound_status
 }
