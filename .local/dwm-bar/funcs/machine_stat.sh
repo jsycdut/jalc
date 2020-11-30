c1a9cfe20b20e281205e604a1272953df74414c9
jsycdut <jsycdut@gmail.com>
2020-11-30 10:18:34 +0800

    fix: fix dwm_bar machine state display issue

--- .local/dwm-bar/funcs/machine_stat.sh
+++ .local/dwm-bar/funcs/machine_stat.sh
@@ -9,14 +9,25 @@
 # just some simple string combination, dont worry
 
 function machine_state() {
-  battery_status="$(cat /sys/class/power_supply/BAT0/status | tr [:lower:] [:upper:])"
-  if [[ "CHARGING" == "$battery_status" ]]; then
-    battery_status='🔺'
+  local battery_file
+  local battery_capacity
+  local battery_display_str
+  local battery_status="⚡"
+  local battery_candidate1=/sys/class/power_supply/BAT0/
+  local battery_candidate1=/sys/class/power_supply/BAT1/
+  [[ -d $battery_candidate1 ]] && battery_file=$battery_candidate1
+  [[ -d $battery_candidate2 ]] && battery_file=$battery_candidate2
+  if [[ -n $battery_file ]]; then
+    battery_status="$(cat $battery_file/status | tr [:lower:] [:upper:])"
+    [[ "CHARGING" == "$battery_status" ]] && battery_status='🔺' || battery_status='🔻'
+    battery_capacity=$(cat $battery_file/capacity)
+    battery_display_str="$battery_status""$battery_capacity"
   else
-    battery_status='🔻'
+    battery_display_str=$battery_status
   fi
-  battery_status="$battery_status$(cat /sys/class/power_supply/BAT0/capacity)"
+
   brightness_status="💡$(printf "%.0f\n" $(xbacklight))"
   sound_status="🔔$(amixer get Master | tail -n 1 | sed -r "s/.*\[(.*)%\].*/\1/")"
-  printf "%s %s %s\n" $battery_status $brightness_status $sound_status
+
+  printf "%s %s %s\n" $battery_display_str $brightness_status $sound_status
 }
