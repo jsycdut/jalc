937acf2a9ac54438238e5bcc49f08908cc63520e
jsycdut <jsycdut@gmail.com>
2021-03-10 16:33:59 +0800

    refactor: refactor script add-super-power.sh for a better use

--- .config/misc/add-super-power.sh
+++ .config/misc/add-super-power.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-sudo echo "jsy ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/jsy
+PRIVILEGE_UNSUFFICIENT=86
+
+if [[ $EUID -ne 0 ]]; then
+  echo "need to be run as sudo or be root."
+  exit $PRIVILEGE_UNSUFFICIENT
+fi
+
+# try to add user jsy and make it a superpower user
+id jsy > /dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+  useradd jsy -m -s /bin/bash
+  passwd jsy
+  echo "jsy ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/jsy
+  echo "jsy is successfully added as a superpower user"
+else
+  echo "no need to do anything, jsy is alreay in this system"
+fi
+
