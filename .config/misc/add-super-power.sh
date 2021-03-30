5a869fa40636da429b9136cf1e9df01ce8265317
jsycdut <jsycdut@gmail.com>
2021-03-31 00:03:19 +0800

    feat: update add-super-power.sh
    add ssh key

--- .config/misc/add-super-power.sh
+++ .config/misc/add-super-power.sh
@@ -11,6 +11,7 @@ fi
 id jsy > /dev/null 2>&1
 if [[ $? -ne 0 ]]; then
   useradd jsy -m -s /bin/bash
+  echo "set password for user jsy"
   passwd jsy
   echo "jsy ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/jsy
   echo "jsy is successfully added as a superpower user"
@@ -18,3 +19,9 @@ else
   echo "no need to do anything, jsy is alreay in this system"
 fi
 
+# change to jsy and add my public key
+su - jsy
+[[ ! -d "/home/jsy/.ssh" ]] && mkdir /home/jsy/.ssh
+cat >> /home/jsy/.ssh/authorized_keys <<EOF
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDalGLpFH4kY+8IdH4NOAWJMJwoRFn6vCuV1t52rjr1SU5vQk1vAXR3+NHDvYuCYp2ysfqxK7HG3b9rfh7caIsE+jM25GT1ksJLtJFcvD87feRAoE3uzcQ93QO6yq5Cwnd/mFcobi8d8w5c5hMf+ZVd8u3D0snYp4Uo98oDE7YdRp+T/L5hWA/SQCaUYptXNas4lqMZuGymdbkTwjGHTRWxPZdn+yh6/+lNOe6+HOx6MUqx74DPlFVqo7WW99+B+zii29Ehc69OYVyDJRvdcJ8IFDNTHNFfJniA6I1CVLcSlw7S/lnikxumNfWe2d1mrExYHpev01LVWW2pBAU60OAJ961GVTe+SJN/SaVMbIiRqTTybMVGLo5Ygy7AN2uqLkncdJbVLcBTgNJIzpbLYZXX8aHXH+V+uIHmzaBGuGfp3Zy3yj2Q8l9+isJZSqf/GnOmsd0pzAIh9U8+XftC2WA7Is8hIO1tQoh5bs2pqFS9FXLkaDVjmpElRhpcgux6kzs= corona@cas
+EOF
